ebe68ff6aa1d8f96c1053016304fe66238e0bb64
Joao Luis Agueda Moreira <joao.moreira@Joaos-MacBook-Pro.local>
2018-07-03 10:43:10 +0100

    welcome to cv

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome' => 'pages#home'
+  get 'cv' => 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
